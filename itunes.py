8b378d9aca5671f21d230b110c90c402fcac838b
tmdvltuf123 <tmdvltuf123@gmail.com>
2026-05-19 23:54:49 +0900

    .

--- itunes.py
+++ itunes.py
@@ -1,15 +1,20 @@
 import requests
+import pandas as pd 
 
 music_item_list_url = "https://itunes.apple.com/search"
-
-
 music_item_list_params = {
-    "term": "아이유",          
-    "country": "KR",        
-    "media": "music",       
-    "limit": 10,             
-    "lang": "ko_kr"          #
+    "term": "아이유",
+    "country": "KR",
+    "media": "music",
+    "limit": 10,
+    "lang": "ko_kr"
 }
 
 response = requests.get(url=music_item_list_url, params=music_item_list_params)
-print(response.text)
+data = response.json()
+
+
+results = data.get("results", [])
+df = pd.DataFrame(results)[['trackName', 'artistName', 'collectionName']]
+
+print(df)
