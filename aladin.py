7ff0d818bcb70ef2d2ff3829b9b1ac02c2cebb25
tmdvltuf123 <tmdvltuf123@gmail.com>
2026-05-20 14:14:07 +0900

    .

--- aladin.py
+++ aladin.py
@@ -1,50 +1,55 @@
 import requests
-import os
 import pandas as pd
-import time  
+import time
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 ALADIN_OPEN_API_KEY = os.getenv("ALADIN_OPEN_API_KEY")
 
-def get_aladin_bestseller():
+genre_targets = {
+    '소설': '1',
+    '경제/경영': '170',
+    '인문': '656', 
+    '사회': '798',
+    '과학': '987',
+    'IT/컴퓨터': '351',
+    '자기계발': '336'
+}
+
+def get_balanced_bestseller():
+    all_books = []
     url = "https://www.aladin.co.kr/ttb/api/ItemList.aspx"
-    book_list = []
     
-    
-    for i in range(3):
-        start_index = (i * 100) + 1
+    for genre_name, cid in genre_targets.items():
         params = {
             "ttbkey": ALADIN_OPEN_API_KEY,
             "QueryType": "BestSeller",
             "SearchTarget": "Book",
-            "MaxResults": 100,  
-            "start": start_index, 
+            "CategoryId": cid,
+            "MaxResults": 28,
+            "start": 1,
             "output": "js",
             "Version": "20131101"
         }
         
         response = requests.get(url, params=params)
         data = response.json()
+        items = data.get("item", [])
         
-       
-        for item in data.get("item", []):
-            book_list.append({
+        for item in items:
+            all_books.append({
                 "title": item.get("title"),
                 "artist": item.get("author"),
-                "year": item.get("pubDate", "")[:4],
+                "year": int(item.get("pubDate", "2000")[:4]), # 년도를 정수형으로 저장
                 "score": item.get("customerReviewRank"),
                 "category": "book",
-                "keywords": ""
+                "genre": genre_name
             })
+        time.sleep(1)
         
-        
-        time.sleep(0.5)
-    
-    return pd.DataFrame(book_list)
+    return pd.DataFrame(all_books)
 
-
-df_books = get_aladin_bestseller()
-df_books.to_csv("book_data.csv", index=False, encoding='utf-8-sig')
-print(f"--- 총 {len(df_books)}개의 데이터를 성공적으로 저장했습니다! ---")
-print(df_books.head()) 
+df = get_balanced_bestseller()
+df.to_csv("book_data.csv", index=False, encoding='utf-8-sig')
+print("--- 데이터 수집 완료 ---")
